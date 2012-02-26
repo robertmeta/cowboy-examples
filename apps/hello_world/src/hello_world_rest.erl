2fca6bb23ad5c5083ed04019608a05f3659c9a7f
Robert Melton <robert@robertmelton.com>
2012-02-26 05:33:37 -0500

    Adding in http_rest

---
+++ apps/hello_world/src/hello_world_rest.erl
@@ -0,0 +1,18 @@
+-module(hello_world_rest).
+-export([start/0, start/2, stop/0]).
+
+start() ->
+    application:start(cowboy),
+    application:start(hello_world_rest).
+
+start(_Type, _Args) ->
+    Dispatch = [
+        {'_', [{'_', hello_world_rest_handler, []}]}
+    ],
+    cowboy:start_listener(my_http_listener, 1,
+        cowboy_tcp_transport, [{port, 8080}],
+        cowboy_http_protocol, [{dispatch, Dispatch}]
+    ).
+
+stop() ->
+    application:stop(cowboy).
--- startit.bat
+++ startit.bat
@@ -2,6 +2,7 @@ erl ^
  -pa deps/cowboy/ebin ^
  -pa apps/hello_world/ebin ^
  -pa apps/hello_world_ssl/ebin ^
+ -pa apps/hello_world_rest/ebin ^
  -pa apps/echo_get/ebin ^
  -pa apps/echo_post/ebin ^
  -boot start_sasl 
