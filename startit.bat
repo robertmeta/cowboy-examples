e2ec145b1bd4293e5506bbb98674bcc5794463d3
Robert Melton <robert@robertmelton.com>
2012-02-26 05:55:22 -0500

    Chunked added to windows startit

--- startit.bat
+++ startit.bat
@@ -3,6 +3,7 @@ erl ^
  -pa apps/hello_world/ebin ^
  -pa apps/hello_world_ssl/ebin ^
  -pa apps/hello_world_rest/ebin ^
+ -pa apps/hello_world_chunked/ebin ^
  -pa apps/echo_get/ebin ^
  -pa apps/echo_post/ebin ^
  -boot start_sasl 
