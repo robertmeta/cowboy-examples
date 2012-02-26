bc0eaa241e25ee904fdcb72726cce91f594cb614
Robert Melton <robert@robertmelton.com>
2012-02-26 05:23:15 -0500

    Added a little safety to get and post

--- apps/echo_get/src/echo_get_handler.erl
+++ apps/echo_get/src/echo_get_handler.erl
@@ -5,11 +5,18 @@ init({tcp, http}, Req, _Opts) ->
     {ok, Req, undefined_state}.
 
 handle(Req, State) ->
-    GetMsg = case cowboy_http_req:qs_val(<<"echo_get">>, Req) of
-        {undefined, _} -> <<"no echo_get querystring parameter">>;
-        {X, _} -> X
-    end, 
-    {ok, Reply} = cowboy_http_req:reply(200, [], ["echo_get: ", GetMsg], Req),
+    Reply = case cowboy_http_req:method(Req) of 
+        {'GET', _} -> 
+            GetMsg = case cowboy_http_req:qs_val(<<"echo_get">>, Req) of
+                {undefined, _} -> <<"no echo_get querystring parameter">>;
+                {X, _} -> X
+            end, 
+            {ok, R} = cowboy_http_req:reply(200, [], ["echo_get: ", GetMsg], Req),
+            R;
+        _ -> 
+            {ok, R} = cowboy_http_req:reply(200, [], <<"Not GET'd">>, Req),
+            R
+    end,
     {ok, Reply, State}.
 
 terminate(_Req, _State) ->
--- apps/echo_post/src/echo_post_handler.erl
+++ apps/echo_post/src/echo_post_handler.erl
@@ -5,11 +5,20 @@ init({tcp, http}, Req, _Opts) ->
     {ok, Req, undefined_state}.
 
 handle(Req, State) ->
-    PostMsg = case cowboy_http_req:body_qs(<<"echo_post">>, Req) of
-        {undefined, _} -> <<"no echo_post querystring parameter">>;
-        {X, _} -> X
-    end, 
-    {ok, Reply} = cowboy_http_req:reply(200, [], ["echo_post: ", PostMsg], Req),
+    Reply = case cowboy_http_req:method(Req) of 
+        {'POST', _} -> 
+            PostMsg = case cowboy_http_req:body_qs(Req) of
+                {X, _} -> case proplists:get_value(<<"echo_post">>, X) of 
+                    undefined -> <<"no echo_post post parameter">>;
+                    Y -> Y
+                end
+            end,
+            {ok, R} = cowboy_http_req:reply(200, [], [<<"echo_post: ">>, PostMsg], Req),
+            R;
+        _ -> 
+            {ok, R} = cowboy_http_req:reply(200, [], <<"Not POST'd">>, Req),
+            R
+    end,
     {ok, Reply, State}.
 
 terminate(_Req, _State) ->
